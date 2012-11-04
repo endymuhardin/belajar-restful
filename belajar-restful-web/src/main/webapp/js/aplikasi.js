93452680f28ae760fdb37020bf820a64c8f37224
Endy Muhardin <endy.muhardin@gmail.com>
2012-11-04 23:03:36 +0700

    implement service find all configs

--- belajar-restful-web/src/main/webapp/js/aplikasi.js
+++ belajar-restful-web/src/main/webapp/js/aplikasi.js
@@ -1,13 +1,24 @@
-var modul = angular.module('belajar', [])
+var modul = angular.module('belajar', ['ngResource'])
     .config(['$routeProvider', function($routeProvider){
         $routeProvider
             .when('/', {templateUrl: 'pages/home.html'})
-            .when('/system/config', {templateUrl: 'pages/system/config.html'})
+            .when('/system/config', {templateUrl: 'pages/system/config.html', controller: 'ApplicationConfigController'})
             .when('/about', {templateUrl: 'pages/about.html', controller: 'AboutController'})
             .otherwise({templateUrl: 'pages/404.html'});
     }]);
+
+modul.factory('ApplicationConfigService', function($resource){
+    return $resource('/config/#', {}, {
+        findAll: {method: 'GET', isArray: true}
+    });
+});
 
 modul.controller('AboutController', function($scope){
     $scope.appName = "Aplikasi Belajar";
     $scope.appVersion = "Versi 1.0.0";
 });
+
+modul.controller('ApplicationConfigController', function($scope, ApplicationConfigService){
+    $scope.configs = ApplicationConfigService.findAll();
+});
+
