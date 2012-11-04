14571ad6179a8f9ee86fa7b8a67e402d94e745f4
Endy Muhardin <endy.muhardin@gmail.com>
2012-11-04 21:18:13 +0700

    controller dimasukkan ke dalam modul, jangan berkeliaran di global scope

--- belajar-restful-web/src/main/webapp/js/aplikasi.js
+++ belajar-restful-web/src/main/webapp/js/aplikasi.js
@@ -1,12 +1,12 @@
-angular.module('belajar', [])
+var modul = angular.module('belajar', [])
     .config(['$routeProvider', function($routeProvider){
         $routeProvider
             .when('/', {templateUrl: 'pages/home.html'})
-            .when('/about', {templateUrl: 'pages/about.html', controller: AboutController})
+            .when('/about', {templateUrl: 'pages/about.html', controller: 'AboutController'})
             .otherwise({templateUrl: 'pages/404.html'});
     }]);
 
-function AboutController($scope){
+modul.controller('AboutController', function($scope){
     $scope.appName = "Aplikasi Belajar";
     $scope.appVersion = "Versi 1.0.0";
-}
+});
