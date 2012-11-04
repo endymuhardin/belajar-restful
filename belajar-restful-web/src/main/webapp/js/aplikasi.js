c6d82dde8d6f796122136652918a5433426080d5
Endy Muhardin <endy.muhardin@gmail.com>
2012-11-04 21:23:55 +0700

    add page untuk edit config

--- belajar-restful-web/src/main/webapp/js/aplikasi.js
+++ belajar-restful-web/src/main/webapp/js/aplikasi.js
@@ -2,6 +2,7 @@ var modul = angular.module('belajar', [])
     .config(['$routeProvider', function($routeProvider){
         $routeProvider
             .when('/', {templateUrl: 'pages/home.html'})
+            .when('/system/config', {templateUrl: 'pages/system/config.html'})
             .when('/about', {templateUrl: 'pages/about.html', controller: 'AboutController'})
             .otherwise({templateUrl: 'pages/404.html'});
     }]);
