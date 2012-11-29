f68c36c83f8e13778e8aa1a96e7d54adde506ef1
Endy Muhardin <endy.muhardin@gmail.com>
2012-11-29 13:09:54 +0700

    remove variabel yang tidak terpakai

--- belajar-restful-web/src/main/java/com/artivisi/belajar/restful/ui/controller/MenuController.java
+++ belajar-restful-web/src/main/java/com/artivisi/belajar/restful/ui/controller/MenuController.java
@@ -25,7 +25,9 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -88,11 +90,8 @@ public class MenuController {
 
     @RequestMapping(value = "/menu", method = RequestMethod.GET)
     @ResponseBody
-    public List<Menu> findAll(
-            Pageable pageable,
-            HttpServletResponse response) {
-
-        return belajarRestfulService.findAllMenu(pageable).getContent();
+    public Page<Menu> findAll(Pageable pageable) {
+        return belajarRestfulService.findAllMenu(pageable);
 
     }
 
