c0a5a9e541a869f97376ac1629ccac942e314f6c
Endy Muhardin <endy.muhardin@gmail.com>
2014-04-29 21:09:39 +0700

    fix indentation

--- belajar-restful-web/yo/app/scripts/controllers/menubar.js
+++ belajar-restful-web/yo/app/scripts/controllers/menubar.js
@@ -3,7 +3,7 @@
 angular.module('yoApp')
   .controller('MenubarCtrl', function ($scope) {
     $scope.userinfo = {
-        "user": "endy",
-        "group": "admin"
+      'user': 'endy',
+      'group': 'admin'
     };
   });
