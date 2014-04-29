fc3e3b8687fc488f3b50677d6b118d875cd1cfaf
Endy Muhardin <endy.muhardin@gmail.com>
2014-04-29 17:25:26 +0700

    fix build issue

--- belajar-restful-web/yo/karma.conf.js
+++ belajar-restful-web/yo/karma.conf.js
@@ -46,9 +46,8 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
-
-
+    browsers: ['Chrome'],
+    
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
     singleRun: false
